c4ca1562a82e4658a6e17791f534ee611552f9fa
Mak4552 <74363176+Mak4552@users.noreply.github.com>
2020-12-18 16:00:19 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,12 @@
+class SlingShot{
+    constructor(body1,body2){
+      var options= { 
+            bodyA:body1,
+            bodyB:body2}
+            this.chain=Constraint.create(options);
+            World.add(world,this.chain);
+    }
+   display(){
+    line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);   
+   }
+} 
